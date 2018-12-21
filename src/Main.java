7084bb755c75d08401a7018d5c6cdb439b4706e1
Polina Chertsova <Polina.Chertsova@sidenis.com>
2018-12-21 14:56:30 +0700

    Initialize array of ints and print all elements by foreach loop

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+
         System.out.println("Hello World in branch!");
+
+        int arr[] = {1, 2, 3};
+
+        for (int el: arr) {
+            System.out.println(el);
+        }
     }
 }
